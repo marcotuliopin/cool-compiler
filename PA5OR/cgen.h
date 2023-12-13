8cae6436b6a250b1d94e81449d61e166adee3dc8
marcotuliopin <marcotuliopin@gmail.com>
2023-12-13 18:58:18 -0300

    ...

--- PA5OR/cgen.h
+++ PA5OR/cgen.h
@@ -1,10 +1,9 @@
 #include <assert.h>
 #include <stdio.h>
+#include <vector>
 #include "emit.h"
 #include "cool-tree.h"
 #include "symtab.h"
-
-#include <map>
 
 enum Basicness     {Basic, NotBasic};
 #define TRUE 1
@@ -18,76 +17,77 @@ typedef CgenNode *CgenNodeP;
 
 class CgenClassTable : public SymbolTable<Symbol,CgenNode> {
 private:
-   List<CgenNode> *nds;
-   ostream& str;
-   int stringclasstag;
-   int intclasstag;
-   int boolclasstag;
-   int ioclasstag;
-   int objectclasstag;
+    List<CgenNode> *nds;
+    ostream& str;
+    int stringclasstag;
+    int intclasstag;
+    int boolclasstag;
 
-   int tagcount;
+    int tagcount;
 
-   std::map<Symbol, int> classtags;
+    std::map<Symbol, int> classtags;
 
 
-// The following methods emit code for
-// constants and global declarations.
+    // The following methods emit code for
+    // constants and global declarations.
 
-   void code_global_data();
-   void code_global_text();
-   void code_bools(int);
-   void code_select_gc();
-   void code_constants();
+    void code_global_data();
+    void code_global_text();
+    void code_bools(int);
+    void code_select_gc();
+    void code_constants();
 
-   void emit_nametab();
+    void code_class_name_tab();
+    void code_class_parent_tab();
+    void code_class_obj_tab();
+    void code_dispatch_tables();
+    void code_prototypes();
+    void code_initializers();
+    void code_methods();
 
+    // The following creates an inheritance graph from
+    // a list of classes.  The graph is implemented as
+    // a tree of `CgenNode', and class names are placed
+    // in the base class symbol table.
 
-// The following creates an inheritance graph from
-// a list of classes.  The graph is implemented as
-// a tree of `CgenNode', and class names are placed
-// in the base class symbol table.
-
-   void install_basic_classes();
-   void install_class(CgenNodeP nd);
-   void install_classes(Classes cs);
-   void build_inheritance_tree();
-   void set_relations(CgenNodeP nd);
-
+    void install_basic_classes();
+    void install_class(CgenNodeP nd);
+    void install_classes(Classes cs);
+    void build_inheritance_tree();
+    void set_relations(CgenNodeP nd);
 public:
-   CgenClassTable(Classes, ostream& str);
-   void code();
-   CgenNodeP root();
+    CgenClassTable(Classes, ostream& str);
+    void code();
+    CgenNodeP root();
 };
 
 
 class CgenNode : public class__class {
-private: 
-   CgenNodeP parentnd;                        // Parent of class
-   List<CgenNode> *children;                  // Children of class
-   Basicness basic_status;                    // `Basic' if class is basic
-                                              // `NotBasic' otherwise
+private:
+    CgenNodeP parentnd;                        // Parent of class
+    List<CgenNode> *children;                  // Children of class
+    Basicness basic_status;                    // `Basic' if class is basic
+                                               // `NotBasic' otherwise
 
 public:
-   CgenNode(Class_ c,
-            Basicness bstatus,
-            CgenClassTableP class_table);
+    CgenNode(Class_ c,
+             Basicness bstatus,
+             CgenClassTableP class_table);
 
-   void add_child(CgenNodeP child);
-   List<CgenNode> *get_children() { return children; }
-   void set_parentnd(CgenNodeP p);
-   CgenNodeP get_parentnd() { return parentnd; }
-   int basic() { return (basic_status == Basic); }
+    void add_child(CgenNodeP child);
+    List<CgenNode> *get_children() { return children; }
+    void set_parentnd(CgenNodeP p);
+    CgenNodeP get_parentnd() { return parentnd; }
+    int basic() { return (basic_status == Basic); }
 };
 
-class BoolConst 
-{
- private: 
-  int val;
- public:
-  BoolConst(int);
-  void code_def(ostream&, int boolclasstag);
-  void code_ref(ostream&) const;
+class BoolConst {
+private:
+    int val;
+public:
+    BoolConst(int);
+    void code_def(ostream&, int boolclasstag);
+    void code_ref(ostream&) const;
 };
 
 class Environment {
